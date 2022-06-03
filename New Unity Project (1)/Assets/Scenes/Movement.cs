2fde75d5d8a0a16e7c309d76b6769b03276a8f60
Владислав161 <64612715+apex161rus@users.noreply.github.com>
2022-06-03 04:53:01 +0300

    Comit 1
    test Prodqect 1

---
+++ New Unity Project (1)/Assets/Scenes/Movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    [SerializeField] private float _speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(_speed * Time.deltaTime,0,0);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(_speed * Time.deltaTime * -1, 0, 0);
+        }
+    }
+}
---
+++ New Unity Project (1)/Assets/Scenes/Target.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Target : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _renderer;
+    [SerializeField] private Slider _slider;
+    [SerializeField] private Color _RendererColor;
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioClip _audioClip;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Plaer>(out Plaer plaer))
+        {
+            _renderer.color = _RendererColor;
+            _audioSource.clip = _audioClip;
+            _audioSource.Play();
+            _audioSource.volume = 1.0f;
+            _slider.value = _audioSource.volume;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Plaer>(out Plaer plaer))
+        {
+            StopCoroutine(test());
+            _renderer.color = Color.green;
+            Debug.Log(Time.deltaTime);
+            StartCoroutine(test());
+        }
+    }
+
+    private IEnumerator test()
+    {
+        for (int i = 0; i < 990; i++)
+        {
+            Debug.Log(Time.deltaTime);
+            _audioSource.volume -= 0.001f;
+            _slider.value = _audioSource.volume;
+            yield return null;
+        }
+    }
+}
