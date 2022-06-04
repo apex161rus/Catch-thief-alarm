807a369627e4b7e8de1aa59aa858735b9f17ba64
Владислав161 <64612715+apex161rus@users.noreply.github.com>
2022-06-04 05:08:50 +0300

    Update Target.cs

--- New Unity Project (1)/Assets/Scenes/Target.cs
+++ New Unity Project (1)/Assets/Scenes/Target.cs
@@ -10,39 +10,45 @@ public class Target : MonoBehaviour
     [SerializeField] private AudioSource _audioSource;
     [SerializeField] private AudioClip _audioClip;
 
+    private IEnumerator _enumerator1;
+
+    private void Start()
+    {
+        _slider.value = 0;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.TryGetComponent<Plaer>(out Plaer plaer))
         {
-            StopCoroutine(TurnDownVolume());
-            _renderer.color = _rendererColor;
+            _renderer.color = Color.red;
             _audioSource.clip = _audioClip;
             _audioSource.Play();
             _audioSource.volume = 1.0f;
             _slider.value = _audioSource.volume;
+            StopCoroutine(_enumerator1);
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
+        _enumerator1 = TurnDownVolume();
+
         if (collision.TryGetComponent<Plaer>(out Plaer plaer))
         {
-            StopCoroutine(TurnDownVolume());
             _renderer.color = Color.green;
-            //Debug.Log(Time.deltaTime);
-            StartCoroutine(TurnDownVolume());
+            StartCoroutine(_enumerator1);
         }
     }
 
     private IEnumerator TurnDownVolume()
     {
-        for (int i = 0; i < 990; i++)
+        for (int i = 0; i < 500; i++)
         {
             Debug.Log(Time.deltaTime);
-            _audioSource.volume -= 0.001f;
+            _audioSource.volume -= 0.002f;
             _slider.value = _audioSource.volume;
             yield return null;
-            //yield return null;
         }
     }
 }
