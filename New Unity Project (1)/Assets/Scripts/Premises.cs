6908da6458f276533b6da917587f54ec61e5e679
Владислав161 <64612715+apex161rus@users.noreply.github.com>
2022-06-16 05:41:35 +0300

    test 10

---
+++ New Unity Project (1)/Assets/Scripts/Premises.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Signaling))]
+[RequireComponent(typeof(BoxCollider2D))]
+
+public class Premises : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _renderer;
+
+    private BoxCollider2D _box;
+    private Signaling _sren;
+
+    private void Start()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+        _box = GetComponent<BoxCollider2D>();
+        _sren = GetComponent<Signaling>();
+        _box.isTrigger = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Player>(out Player player))
+        {
+            _renderer.color = Color.red;
+            _sren.MaxVolume();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Player>(out Player player))
+        {
+            _renderer.color = Color.green;
+            _sren.MinVolume();
+        }
+    }
+}
---
+++ New Unity Project (1)/Assets/Scripts/Signaling.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class Signaling : MonoBehaviour
+{
+    [Tooltip("Insert audio signal")]
+    [SerializeField] private AudioClip _audioClip;
+
+    private AudioSource _audioSource;
+    private Coroutine _coroutine;
+
+    public void MaxVolume()
+    {
+        float maxVolume = 1f;
+        CheckForStopAttempt(_coroutine);
+        _coroutine = StartCoroutine(RenameVolume(maxVolume));
+    }
+
+    public void MinVolume()
+    {
+        float minVolume = 0f;
+        CheckForStopAttempt(_coroutine);
+        _coroutine = StartCoroutine(RenameVolume(minVolume));
+    }
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.clip = _audioClip;
+        _audioSource.volume = 0;
+        _audioSource.Play();
+    }
+
+    private IEnumerator RenameVolume(float volume)
+    {
+        while (_audioSource.volume != volume)
+        {
+            float speed = 0.1f;
+            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, volume, speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    private void CheckForStopAttempt(Coroutine coroutine)
+    {
+        if (coroutine != null)
+        {
+            StopCoroutine(coroutine);
+        }
+    }
+}
